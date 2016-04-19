5fa783eb720d363245626c855aaeb8bc629382bb
andres-root <andres.root.coder@gmail.com>
2016-04-18 23:46:09 -0500

    working

--- src/www/views.py
+++ src/www/views.py
@@ -1,7 +1,16 @@
 
-from django.shortcuts import render
+# from django.shortcuts import render
 from django.http import HttpResponse
+from .api import Twitter
+from .models import Tweet
 
 
 def index(request):
-    return HttpResponse('Hello, this is penélope!')
+    try:
+        twitter = Twitter()
+        twitter.stream('amor')
+        tweets = Tweet.objects.all()
+        return HttpResponse(tweets[len(tweets) - 1].text)
+    except Exception:
+        import ipdb; ipdb.set_trace()
+        # return HttpResponse('ERROR')
